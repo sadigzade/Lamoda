cfe3d31571545bd62f7394a9095f157044ea0e29
Sadigzade Hikmet <sadigzade.hi@mail.ru>
2021-07-09 12:03:27 +0300

    Начал работать раздел с товарами

--- scripts/script.js
+++ scripts/script.js
@@ -60,11 +60,11 @@ const getData = async () => {
     }
 }
 
-const getGoods = (callback, value) => {
+const getGoods = (callback, prop, value) => {
     getData()
         .then(data => {
             if (value) {
-                callback(data.filter(item => item.category === value))
+                callback(data.filter(item => item[prop] === value))
             } else {
                 callback(data)
             }
@@ -74,11 +74,29 @@ const getGoods = (callback, value) => {
         })
 }
 
+subheaderCart.addEventListener('click', cartModalCartOpen)
+
+cartOverlay.addEventListener('click', event => {
+    const target = event.target
+
+    if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+        cartModalCartClose()
+    }
+})  
+
+// Страница категорий
+
 try {
     const goodsList = document.querySelector('.goods__list')
 
     if (!goodsList) {
         throw 'This is not a goods page!'
+    }
+
+    const goodsTitle = document.querySelector('.goods__title')
+
+    const changeTitle = () => {
+        goodsTitle.textContent = document.querySelector(`[href*="#${hash}"]`).textContent
     }
 
     const createCard = ({ id, preview, cost, brand, name, sizes }) => {        
@@ -117,21 +135,82 @@ try {
     
     window.addEventListener('hashchange', () => {
         hash = location.hash.substring(1)
-        getGoods(renderGoodsList, hash)
+        getGoods(renderGoodsList, 'category', hash)
+        changeTitle()
     })
     
-    getGoods(renderGoodsList, hash)
+    changeTitle()
+    getGoods(renderGoodsList, 'category', hash)
 
 } catch (err) {
     console.warn(err)
 }
 
-subheaderCart.addEventListener('click', cartModalCartOpen)
+// Страница товара
 
-cartOverlay.addEventListener('click', event => {
-    const target = event.target
-
-    if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
-        cartModalCartClose()
+try {
+    if (!document.querySelector('.card-good')) {
+        throw 'This is not a cart-good page'
     }
-})  
+    
+    const cardGoodImage = document.querySelector('.card-good__image')
+    const cardGoodBrand = document.querySelector('.card-good__brand')
+    const cardGoodTitle = document.querySelector('.card-good__title')
+    const cardGoodPrice = document.querySelector('.card-good__price')
+    const cardGoodColor = document.querySelector('.card-good__color')
+    const cardGoodSelectWrapper = document.querySelectorAll('.card-good__select__wrapper')
+    const cardGoodColorList = document.querySelector('.card-good__color-list')
+    const cardGoodSizes = document.querySelector('.card-good__sizes')
+    const cardGoodSizesList = document.querySelector('.card-good__sizes-list')
+    const cardGoodBuy = document.querySelector('.card-good__buy')
+
+    const generateList = data => data.reduce((html, item, i) => {
+        return html + `<li class="card-good__select-item" data-id="${i}">${item}</li>`
+    }, '')
+
+    const renderCartGood = ([{ photo, cost, brand, name, sizes, color }]) => {
+        cardGoodImage.src = `goods-image/${photo}`
+        cardGoodImage.alt = `${brand} ${name}`
+        cardGoodBrand.textContent = brand
+        cardGoodTitle.textContent = name
+        cardGoodPrice.textContent = `${cost} ₽`
+
+        if (color) {
+            cardGoodColor.textContent = color[0]
+            cardGoodColor.dataset.id = 0
+            cardGoodColorList.innerHTML = generateList(color)
+        } else {
+            cardGoodColor.style.display = 'none'
+        }
+
+        if (sizes) {
+            cardGoodSizes.textContent = sizes[0]
+            cardGoodSizes.dataset.id = 0
+            cardGoodSizesList.innerHTML = generateList(sizes)
+        } else {
+            cardGoodSizes.style.display = 'none'
+        }
+    }
+
+    cardGoodSelectWrapper.forEach(item => {
+        item.addEventListener('click', e => {   
+            const target = e.target
+
+            if (target.closest('.card-good__select')) {
+                target.classList.toggle('card-good__select__open')
+            }
+
+            if (target.closest('.card-good__select-item')) {
+                const cardGoodSelect = item.querySelector('.card-good__select')
+                cardGoodSelect.textContent = target.textContent
+                cardGoodSelect.dataset.id = target.dataset.id
+                cardGoodSelect.classList.remove('card-good__select__open')
+            }
+        })
+    })
+
+    getGoods(renderCartGood, 'id', hash)
+
+} catch (err) {
+    console.warn(err)
+}
