32c94153a5e8f4b6cb556ff22659fd16a7f4c4fb
Sadigzade Hikmet <sadigzade.hi@mail.ru>
2021-07-10 11:46:20 +0300

    Last commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,48 @@
 const headerCityButton = document.querySelector('.header__city-button')
+const cartListGoods = document.querySelector('.cart__list-goods')
+const cartTotalCost = document.querySelector('.cart__total-cost')
+const subheaderCart = document.querySelector('.subheader__cart')
+const cartOverlay = document.querySelector('.cart-overlay')
 
 let hash = location.hash.substring(1)
 
 headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?'
 
-headerCityButton.addEventListener('click', () => {
-    const city = prompt('Укажите ваш город')
-    headerCityButton.textContent = city
-    localStorage.setItem('lomoda-location', city)
-})
+const getLocalStorage = () => JSON?.parse(localStorage.getItem('cart-lomoda')) || []
+const setLocalStorage = data => localStorage.setItem('cart-lomoda', JSON.stringify(data))
+
+const renderCart = () => {
+    cartListGoods.textContent = ''
+
+    const cartItems = getLocalStorage()
+
+    let totalPrice = 0
+
+    cartItems.forEach((item, i) => {
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
+            <td>${i + 1}</td>
+            <td>${item.brand} ${item.name}</td>
+            ${item.color ? `<td>${item.color}</td>` : '<td>-</td>'}
+            ${item.size ? `<td>${item.size}</td>` : '<td>-</td>'}
+            <td>${item.cost} &#8381;</td>
+            <td><button class="btn-delete" data-id="${item.id}">&times;</button></td>
+        `
+
+        totalPrice += item.cost
+
+        cartListGoods.append(tr)
+    })
+
+    cartTotalCost.textContent = totalPrice + ' ₽'
+}
+
+const deleteItemCart = id => {
+    const cartItems = getLocalStorage()
+    const newCartItems = cartItems.filter(item => item.id !== id)
+    setLocalStorage(newCartItems)
+}
 
 // Блокировка скролла
 
@@ -35,12 +69,10 @@ const enableScroll = () => {
 
 // Модальное окно 
 
-const subheaderCart = document.querySelector('.subheader__cart')
-const cartOverlay = document.querySelector('.cart-overlay')
-
 const cartModalCartOpen = () => {
     cartOverlay.classList.add('cart-overlay-open')
     disableScroll()
+    renderCart()
 }
 
 const cartModalCartClose = () => {
@@ -73,6 +105,19 @@ const getGoods = (callback, prop, value) => {
             console.error(err)
         })
 }
+
+headerCityButton.addEventListener('click', () => {
+    const city = prompt('Укажите ваш город')
+    headerCityButton.textContent = city
+    localStorage.setItem('lomoda-location', city)
+})
+
+cartListGoods.addEventListener('click', e => {
+    if (e.target.matches('.btn-delete')) {
+        deleteItemCart(e.target.dataset.id)
+        renderCart()
+    }
+})
 
 subheaderCart.addEventListener('click', cartModalCartOpen)
 
@@ -168,7 +213,9 @@ try {
         return html + `<li class="card-good__select-item" data-id="${i}">${item}</li>`
     }, '')
 
-    const renderCartGood = ([{ photo, cost, brand, name, sizes, color }]) => {
+    const renderCartGood = ([{ id, photo, cost, brand, name, sizes, color }]) => {
+        const data = { brand, name, cost, id }
+
         cardGoodImage.src = `goods-image/${photo}`
         cardGoodImage.alt = `${brand} ${name}`
         cardGoodBrand.textContent = brand
@@ -190,6 +237,30 @@ try {
         } else {
             cardGoodSizes.style.display = 'none'
         }
+
+        if (getLocalStorage().some(item => item.id === id)) {
+            cardGoodBuy.classList.add('delete')
+            cardGoodBuy.textContent = 'Удалить из корзины'
+        }
+
+        cardGoodBuy.addEventListener('click', () => {
+            if (cardGoodBuy.classList.contains('delete')) {
+                deleteItemCart(id)
+                cardGoodBuy.classList.remove('delete')
+                cardGoodBuy.textContent = 'Добавить в корзину'
+                return
+            }
+
+            if (color) data.color = cardGoodColor.textContent 
+            if (sizes) data.size = cardGoodSizes.textContent
+
+            cardGoodBuy.classList.add('delete')
+            cardGoodBuy.textContent = 'Удалить из корзины'
+
+            const cardData = getLocalStorage()
+            cardData.push(data)
+            setLocalStorage(cardData)
+        })
     }
 
     cardGoodSelectWrapper.forEach(item => {
